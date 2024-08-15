b55353d000f11c7f4f1191160d2b35578f8b25e4
kevinliu399 <kevinliu399@gmail.com>
2024-08-15 01:04:15 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <stack>
+using namespace std;
+
+// use & to pass by reference
+bool matchingBrackets(ifstream& file) {
+    stack<char> bracketStack;
+    int validBracket = 0;
+    char ch;
+
+    while (file.get(ch)) {
+        if (ch == '{') {
+            bracketStack.push(ch);
+        } else if (ch == '}') {
+            if (bracketStack.empty()) {
+                return false; // no matching opening bracket
+            } else {
+                bracketStack.pop();
+                validBracket++;
+            }
+        }
+    }
+
+    return bracketStack.empty();
+}
+
+int main () {
+  string line;
+  ifstream myfile ("test.txt"); // ifstream -> read from stream
+  bool isMatching;
+  if (myfile.is_open())
+  {
+    isMatching = matchingBrackets(myfile);
+    cout << "Matching brackets: " << isMatching << endl;
+    myfile.close();
+  }
+
+  else cout << "Unable to open file"; 
+
+  return 0;
+}
